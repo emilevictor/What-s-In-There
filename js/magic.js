4f7458932f071ee4e2ee971c3f25ac0ecf2a5c2a
Emile Victor <emilevictor@gmail.com>
2012-10-03 17:31:18 +1000

    All files committed

--- js/magic.js
+++ js/magic.js
@@ -1,12 +1,23 @@
 var gRoomData = null
 var $xml = null
 var xmlDoc = null
+var coursesArray = null
+var soWittyIndex = 0;
+var soWitty = ['Now powered by magical <a href="http://www.uqrota.net">UQRota</a> dust!',
+				'Now with 10% less soylent green in every refec meal!',
+				'By Emile Victor, Michael Mallon & Alex Wilson']
 
 $(document).ready(function() {
 	$("#buildingRoomQuery").hide();
 	$("#errorMessage").hide();
 	$("#results").hide();
-
+	$("#header").hide();
+	$("#header").show("slide", {direction: "up"},200);
+	$("#lulzyMessage").hide();
+	soWittyIndex = Math.floor(Math.random()*soWitty.length);
+	$("#lulzyMessage").append(soWitty[soWittyIndex]);
+	setTimeout("showLulzyMessage()",500);
+	var timer = setInterval("showNextMessage()",15000);
 	$("#buildingRoomQuery").show("slide", {direction: "up"},200)
 
 
@@ -62,16 +73,72 @@ $(document).ready(function() {
 		var todayShortDate = days[dayIndex-1]
 		console.log(todayShortDate)
 
+		var classes = []
+
 		$xml.find('session').each(function() {
-			console.log($(this).find('day').text())
+
 			if ($(this).find('day').text() == todayShortDate)
 			{
-				console.log("OHAI")
-				$("#results").append("<div class=\"indivClass\"><p class=\"className\">Class: "+$(this).find('group series offering course').text()+"</p> <p class=\"day\">"+$(this).find('day').text() + " | " + $(this).find('start').text()+" - "+$(this).find('finish').text()+"</p></div>")
+				var classObject = {}
+				var startDate = Date.parse($(this).find('start').text());
+				var finishDate = Date.parse($(this).find('finish').text());
+
+				if (startDate == null || finishDate == null)
+				{
+					console.log("startDate or finishDate null for " + $(this).find('group series offering course').text())
+				}
+				classObject.course = $(this).find('group series offering course').text()
+				classObject.startDate = startDate
+				classObject.finishDate = finishDate
+				classes.push(classObject);
 			} else {
-				console.log($(this).find('day').text() + " vs " + todayShortDate)
+				//This class is not today's.
 			}
 		})
+
+		//Sort the array
+
+		classes.sort(function(a, b) {
+			if (a.startDate != null && b.startDate != null)
+			{
+				return a.startDate.compareTo(b.startDate);
+			} else {
+				return 0;
+			}
+		});
+
+		coursesArray = classes
+
+
+		$("#results").empty();
+		$("#results").append("<p class=\"warning\">Please note that the times shown on this page are taken directly from MySI-net... They may (and probably will) contain errors. These are the classes occurring in this room today.");
+
+		for (var i = 0; i < coursesArray.length; i++)
+		{
+			var startMinutes;
+			var finishMinutes;
+			if (coursesArray[i].startDate.getMinutes() == 0)
+			{
+				startMinutes = "00"
+			} else {
+				startMinutes = coursesArray[i].startDate.getMinutes().toString()
+			}
+
+			if (coursesArray[i].finishDate.getMinutes() == 0)
+			{
+				finishMinutes = "00"
+			} else {
+				finishMinutes = coursesArray[i].finishDate.getMinutes().toString()
+			}
+			//$("#results").empty();
+			$("#results").append("<div class=\"indivClass\"><p class=\"className\">Class: "+coursesArray[i].course+"</p> <p class=\"day\">"+days[coursesArray[i].startDate.getDay()-1] + " | " + coursesArray[i].startDate.getHours().toString()+":"+startMinutes+" - "+coursesArray[i].finishDate.getHours().toString() + ":" + finishMinutes)
+		}
+		
+		if (coursesArray.length == 0)
+		{
+			$("#results").empty();
+			$("#results").append("<iframe width=\"420\" height=\"315\" src=\"http://www.youtube.com/embed/4tiPOMd14eQ\" frameborder=\"0\" allowfullscreen></iframe>");
+		}
 
 		var timer = setTimeout("showResults()",200);
 
@@ -87,10 +154,35 @@ $(document).ready(function() {
 
 });
 
+function showLulzyMessage()
+{
+	$("#lulzyMessage").fadeIn('slow');
+}
+
+function showNextMessage() 
+{
+	$("#lulzyMessage").fadeOut('fast', function() {
+		
+		soWittyIndex += 1;
+		if (soWittyIndex == soWitty.length)
+		{
+			soWittyIndex = 0;
+		}
+		$("#lulzyMessage").empty();
+		$("#lulzyMessage").append(soWitty[soWittyIndex]);
+		$("#lulzyMessage").fadeIn('slow')
+
+	});
+
+	
+	
+
+}
+
 function showResults()
 {
 	$("#buildingRoomQuery").hide("slide", {direction: "up"},200)
-	$("#results").show("slide", {direction: "up"},3000);
+	$("#results").show("slide", {direction: "up"},1000);
 }
 
 
